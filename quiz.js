0b22fd311efb87515fb584cc0fb08e08c9f09667
pitó <pit@gmail.com>
2024-06-04 21:52:20 -0300

    segunda alteração

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function init(){
+    const element = document.getElementById("init");
+    element.style.display = "none";
+
+    const element1 = document.getElementById("esc");
+    element1.style.display  = "block";
+  
+    const element2 = document.getElementsByClassName("esc1");
+    element2.style.flex-direction; "column";
+    element2.style.align-items; "center";
+    element2.style.justify-content; "center";
+    
+
+ 
+    
+}
